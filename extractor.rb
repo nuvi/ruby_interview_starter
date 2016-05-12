dade27a1d27555d0cb1e323f727775d9f539660a
Nick Eggertz <neggertz@gmail.com>
2016-05-12 08:56:45 -0600

    Wasn't sure if the extraction was to happen into a file or into a directory, Added extraction into a directory

--- extractor.rb
+++ extractor.rb
@@ -1,13 +1,12 @@
 require 'rubygems'
 require 'zip'
-require 'fileutils'
 
 class Extractor
 	
 	def extract_data
 		Zip::File.open('test/files/news.zip') do |zip_file|
 		  # Handle entries one by one
-			fileStream = File.open('test/files/extract', 'a')
+			fileStream = File.open('test/files/extract_data', 'a')
 			zip_file.each do |entry|
 		   		#Show the user each file being extracted
 		   		puts "Extracting #{entry.name}"
@@ -19,7 +18,21 @@ class Extractor
 			fileStream.close
 		end
 	end
+	def extract_to_folder
+		directory_name = "test/files/extract"
+		Dir.mkdir(directory_name) unless File.exists?(directory_name)
+		
+		Zip::File.open('test/files/news.zip') do |zip_file|
+			zip_file.each do |entry|
+		   		#Show the user each file being extracted
+		   		puts "Extracting #{entry.name}"
+		   		entry.extract(directory_name+"/#{entry.name}")
+		   	
+		   	end
+		end
+	end
 end
 
 extractor = Extractor.new
 extractor.extract_data
+extractor.extract_to_folder
