575cbb0ca69f42b22b1a995a23551fd4e1ae5e9a
Nick Eggertz <neggertz@gmail.com>
2016-05-11 22:55:38 -0600

    changed from instance variables to local variables

--- extractor.rb
+++ extractor.rb
@@ -1,21 +1,22 @@
 require 'rubygems'
 require 'zip'
+require 'fileutils'
 
 class Extractor
 	
 	def extract_data
 		Zip::File.open('test/files/news.zip') do |zip_file|
 		  # Handle entries one by one
-			@fileStream = File.open('test/files/extract', 'a')
+			fileStream = File.open('test/files/extract', 'a')
 			zip_file.each do |entry|
 		   		#Show the user each file being extracted
 		   		puts "Extracting #{entry.name}"
 		   		#Write out from the entries to the stream
-		   		@fileStream.write(entry.get_input_stream.read)
+		   		fileStream.write(entry.get_input_stream.read)
 		   		
 			end
 			#clean up the file stream
-			@fileStream.close
+			fileStream.close
 		end
 	end
 end
