d0b51ec8c8bddf9895a3ccbce5c68669d45a2301
Nick Eggertz <neggertz@gmail.com>
2016-05-11 21:58:35 -0600

    Moved the extraction into a method

--- extractor.rb
+++ extractor.rb
@@ -2,17 +2,23 @@ require 'rubygems'
 require 'zip'
 
 class Extractor
-	Zip::File.open('test/files/news.zip') do |zip_file|
-	  # Handle entries one by one
-		@fileStream = File.open('test/files/extract', 'a')
-		zip_file.each do |entry|
-	   		#Show the user each file being extracted
-	   		puts "Extracting #{entry.name}"
-	   		#Write out from the entries to the stream
-	   		@fileStream.write(entry.get_input_stream.read)
-	   		
+	
+	def extract_data
+		Zip::File.open('test/files/news.zip') do |zip_file|
+		  # Handle entries one by one
+			@fileStream = File.open('test/files/extract', 'a')
+			zip_file.each do |entry|
+		   		#Show the user each file being extracted
+		   		puts "Extracting #{entry.name}"
+		   		#Write out from the entries to the stream
+		   		@fileStream.write(entry.get_input_stream.read)
+		   		
+			end
+			#clean up the file stream
+			@fileStream.close
 		end
-		#clean up the file stream
-		@fileStream.close
 	end
 end
+
+extractor = Extractor.new
+extractor.extract_data
