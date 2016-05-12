5eed31e21dcdcea730d8316bd710dc178a6c1246
thebradhimself <Bradley.Hintze@gmail.com>
2016-05-11 18:32:09 -0600

    unzip

--- hello_world.rb
+++ hello_world.rb
@@ -1,1 +1,20 @@
+require 'rubygems'
+require 'zip'
+require 'pry'
+
+def grab_the_zips()
+  Zip::File.open('test/files/news.zip') do |zip_file|
+    # Handle entries one by one
+    temp_dir = "test/files/extract/"
+    Dir.mkdir(temp_dir) unless File.exists?(temp_dir)
+    zip_file.each do |entry|
+    # Extract to file/directory/symlink
+      puts "Extracting #{entry.name}"
+      final_path = File.join(temp_dir, entry.name)
+      entry.extract(final_path)
+    end
+  end
+end
+
 puts "HELLO WORLD CODE: HR#{Random.rand(5000)}RH"
+grab_the_zips()
