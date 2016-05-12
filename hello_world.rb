8f73994fdd5ab31a922f5a2cf66d96f92aae3ae6
thebradhimself <Bradley.Hintze@gmail.com>
2016-05-11 18:34:45 -0600

    unzip file and offload files in different directory

--- hello_world.rb
+++ hello_world.rb
@@ -4,13 +4,16 @@ require 'pry'
 
 def grab_the_zips()
   Zip::File.open('test/files/news.zip') do |zip_file|
-    # Handle entries one by one
+    # directory for unzipping
     temp_dir = "test/files/extract/"
+    # create directory if it does not exist
     Dir.mkdir(temp_dir) unless File.exists?(temp_dir)
+    # open zip file and read through all files
     zip_file.each do |entry|
-    # Extract to file/directory/symlink
       puts "Extracting #{entry.name}"
+      #creates path using temp dir and the name of the file
       final_path = File.join(temp_dir, entry.name)
+      #saves the file to the directory created earlier with original file name 
       entry.extract(final_path)
     end
   end
