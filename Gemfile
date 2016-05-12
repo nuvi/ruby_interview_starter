a08f56432468d6924e04b905b457b09dac4b6221
Allen Maxwell <aamax@xmission.com>
2016-05-11 18:42:17 -0600

    unzip files to extract

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in ruby_interview_starter.gemspec
 gemspec
+
+gem 'rubyzip', '>= 1.0.0'
---
+++ lib/nuvi_unzip.rb
@@ -0,0 +1,22 @@
+require 'zip'
+
+class NuviUnZip
+  Zip.on_exists_proc = true
+
+  def self.unzip_file(fname)
+    dirname = File.dirname(fname)
+
+    # Dir.mkdir "#{dirname}/extract/"   unless File.exists?("#{dirname}/extract/")
+    dest_file = "#{dirname}/extract"
+
+    Zip::File.open(fname) do |zip_file|
+      # Handle entries one by one
+      zip_file.each do |entry|
+        # Extract to file/directory/symlink
+        puts "Extracting #{entry.name}"
+        entry.extract(dest_file)
+      end
+
+    end
+  end
+end
