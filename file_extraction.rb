ae60247e9804ec7aeb5b0e3448b0ff6c6d2906b3
grandmasterspoc <dilloncortez@gmail.com>
2016-05-11 18:57:00 -0600

    added file extraction from news.zip into test/files/extract

--- file_extraction.rb
+++ file_extraction.rb
@@ -6,6 +6,9 @@ input_dir = 'test/files/extract'
 zipfile_name = 'test/files/news.zip'
 
 Zip::File.open(zipfile_name, Zip::File::CREATE) do |zip_file|
+
+  # never used this before by the way. :)
+
   zip_file.each do |zip|
     puts "extracting #{zip} file"
     input_path = "#{input_dir}/#{zip}"
