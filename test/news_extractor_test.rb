6c57bb10d3a7b5308c073c0ed903cb8f1743eb05
jsquire4 <jacobmsquire@yahoo.com>
2016-05-11 18:24:37 -0600

    finished news extract

--- test/news_extractor_test.rb
+++ test/news_extractor_test.rb
@@ -1,9 +1,27 @@
-require 'test_helper'
+require './test_helper.rb'
+require 'zip'
+require 'pry'
 
 class NewsExtractorTest < Minitest::Test
-
   def text_can_extract_news_xml
     assert false
     puts "TEST CODE: HR#{Random.rand(5000)}RH"
   end
 end
+
+def news
+  n = 0
+  Zip::File.open('./files/news.zip') do |zip_file|
+    zip_file.each do |entry|
+      n +=1
+      puts "Extracting #{entry.name}"
+      entry.extract("./files/extract/extractions#{n}.txt")
+      content = entry.get_input_stream.read
+    end
+  end
+end
+
+news
+bindind.pry
+
+puts 'done'
