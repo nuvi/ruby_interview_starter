86387e43ec4480f5969d8980a6098f988e15c44f
Kaitlin Anderson <kaitlin_anderson@mac.com>
2016-05-12 18:38:57 -0600

    extract files from zip

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in ruby_interview_starter.gemspec
 gemspec
+gem 'rubyzip'
--- lib/ruby_interview_starter.rb
+++ lib/ruby_interview_starter.rb
@@ -1,5 +1,26 @@
-require "ruby_interview_starter/version"
+require "./lib/ruby_interview_starter/version"
+require 'zip'
 
 module RubyInterviewStarter
   # Your code goes here...
+
+  class GetThoseFiles
+    def unzip(zipped_file, directory)
+      Zip::File.open(zipped_file) do |zip_file|
+        # iterations = 10
+        zip_file.each do |entry|
+          entry.extract("#{directory}/#{entry.name}")
+          content = entry.get_input_stream.read
+          # if iterations <= 1
+          #   break
+          # end
+          # iterations -= 1
+        end
+      end
+    end
+  end
 end
+
+
+zipped_files = RubyInterviewStarter::GetThoseFiles.new
+zipped_files.unzip('test/files/news.zip', 'test/files/extract')
