16c050513e1021a7e3a871ec1c80c270f0edbf30
Colin Anderson <andercol.4@gmail.com>
2016-05-13 12:46:44 -0600

    automated, running on a smaller scale to save time

--- lib/ruby_interview_starter.rb
+++ lib/ruby_interview_starter.rb
@@ -13,13 +13,16 @@ module DataManipulator
       site = agent.get(url)
       links = site.links
       links.each_with_index do |l, i|
-        if i >= 5 && i <= 20
+        if i >= 5 && <= 20
           if i%5 == 0
             puts 'still downloading'
           end
           # binding.pry
+          puts 'starting get'
           link = agent.get("#{url}/#{l.href}")
+          puts 'starting save'
           link.save("#{destination}/#{l.text}")
+          puts 'save complete'
         end
       end
     end
@@ -27,14 +30,14 @@ module DataManipulator
     def self.unzip(file, destination)
       Zip::File.open(file, destination) do |zip_file|
         zip_file.each do |entry|
-          entry.extract("#{destination}/#{entry.name}")
+          entry.extract("#{destination}/#{entry.name}") {true}
         end
       end
     end
 
     def self.redis_loader(directory)
       dir = Dir.open(directory)
-      redis = Redis.new(:host => "127.0.0.1", :port => 6379, :db => 1)
+      redis = Redis.new(:host => "127.0.0.1", :port => 6379, :db => 12)
       i = 0
       dir.each do |file|
         extension = file.split('.').last
