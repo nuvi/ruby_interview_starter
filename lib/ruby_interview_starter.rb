103a555849bf4c81d9383a0f2741cd9ac21cf6c2
Colin Anderson <andercol.4@gmail.com>
2016-05-12 20:19:48 -0600

    built automation, but it is slow

--- lib/ruby_interview_starter.rb
+++ lib/ruby_interview_starter.rb
@@ -1,27 +1,28 @@
 # require "ruby_interview_starter/version"
 
-module RubyInterviewStarter
+module DataManipulator
   require 'rubygems'
   require 'zip'
   require 'pry'
   require 'redis'
   require 'mechanize'
 
-  class Downloader
+  class FileManager
     def self.download(url, destination)
       agent = Mechanize.new
       site = agent.get(url)
       links = site.links
       links.each_with_index do |l, i|
-        unless i <= 5
-          link = l.click
-          link.save("destination/#{l.text}")
+        if i >= 5
+          if i%5 == 0
+            puts 'still downloading'
+          end
+          # binding.pry
+          link = agent.get("#{url}/#{l.href}")
+          link.save("#{destination}/#{l.text}")
         end
       end
     end
-  end
-
-  class Converter
 
     def self.unzip(file, destination)
       Zip::File.open(file, destination) do |zip_file|
@@ -31,19 +32,21 @@ module RubyInterviewStarter
       end
     end
 
-    def self.move_to_redis(directory)
+    def self.redis_loader(directory)
       dir = Dir.open(directory)
       redis = Redis.new(:host => "127.0.0.1", :port => 6379, :db => 1)
+      i = 0
       dir.each do |file|
         extension = file.split('.').last
         if extension == 'xml'
+          if i%1000 == 0
+            puts 'pushing on to Redis queue'
+          end
           text = File.read("#{directory}/#{file}")
           redis.set(file, text)
+          i+= 1
         end
-
       end
     end
   end
 end
-include RubyInterviewStarter
-Converter.move_to_redis(ARGV[0])
