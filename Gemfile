23d852fd96d81cc3669d01dd6b7cae2fba25ead1
jrichocean <jrichocean@gmail.com>
2016-05-13 14:56:02 -0600

    STARTS/STASHES changes to new file lib/ruby_bitly_downloader.rb

--- Gemfile
+++ Gemfile
@@ -1,4 +1,9 @@
 source 'https://rubygems.org'
 
+gem 'minitest', '>= 5.0.0'
+gem 'pry'
+gem 'pry-byebug'
+
+
 # Specify your gem's dependencies in ruby_interview_starter.gemspec
 gemspec
---
+++ lib/ruby_bitly_downloader.rb
@@ -0,0 +1,69 @@
+# I know I can use nokogiri to do/simplify this code easily, but I decided why not not use nokogiri... so that is this.
+require 'rubygems'
+require 'net/http'
+require 'uri'
+require 'pry'
+require 'pry-byebug'
+
+
+class RubyBitlyDownloader
+  # Establishes class constants
+  PAGE_URL = 'http://feed.omgili.com/5Rh5AMTrc4Pv/mainstream/posts/'
+
+  def main
+    response = get_response(PAGE_URL)
+    parsed_response = parse_response(response)
+    cleaned_response = clean_up_response_array(parsed_response)
+    file_name_array = create_array_of_file_names(cleaned_response)
+    uri_array = create_array_of_uri_obj(file_name_array)
+    name_hash = zip_and_hash(uri_array, file_name_array)
+    downloaded_files = download_files(name_hash)
+  end
+
+  def get_response url
+    uri = URI.parse(url)
+    response = Net::HTTP.get_response(uri).body
+  end
+
+  def parse_response arg
+    @response_array = []
+    arg.each_line do |line|
+      @response_array << line
+    end
+    return @response_array
+  end
+
+  def clean_up_response_array arg
+    arg.keep_if do |str|
+      str[0..9] == "<tr><td><a"
+    end
+  end
+
+  def create_array_of_file_names arr
+    arr.map do |x|
+      x.split(/"([^"]+)"/)[1]
+    end
+  end
+
+  def create_array_of_uri_obj arr
+    arr.drop(1)
+    arr.map do |i|
+      URI("http://feed.omgili.com/5Rh5AMTrc4Pv/mainstream/posts/" + i)
+    end
+  end
+
+  def zip_and_hash file_name_array, uri_array
+    @file_hash = file_name_array.zip(uri_array).to_h
+  end
+
+  def download_files file_hash
+    file_hash.each do |k, v|
+      res = Net::HTTP.get(k)
+      File.open(File.expand_path('../test/files/downloads/', '#{v}'), 'w+') do |file|
+        file.write(res)
+      end
+    end
+  end
+
+end
+
