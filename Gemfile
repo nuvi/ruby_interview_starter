fc9f3ceebf2e56c69e971816b0a5e76f98e74926
Kaitlin Anderson <kaitlin_anderson@mac.com>
2016-05-13 20:00:38 -0600

    download zip files from url

--- Gemfile
+++ Gemfile
@@ -3,3 +3,4 @@ source 'https://rubygems.org'
 # Specify your gem's dependencies in ruby_interview_starter.gemspec
 gemspec
 gem 'rubyzip'
+gem 'mechanize'
--- lib/ruby_interview_starter.rb
+++ lib/ruby_interview_starter.rb
@@ -1,5 +1,6 @@
 require "./lib/ruby_interview_starter/version"
 require 'zip'
+require 'mechanize'
 
 module RubyInterviewStarter
   # Your code goes here...
@@ -19,8 +20,30 @@ module RubyInterviewStarter
       end
     end
   end
+
+  class DownloadFiles
+    def scrape
+      agent = Mechanize.new
+      agent.pluggable_parser.default = Mechanize::Download
+      page = agent.get('http://bitly.com/nuvi-plz')
+      # iterations = 10
+      page.links.each do |link|
+        next unless /\.zip$/ =~ link.text
+        # download files to test/files/downloads (deleted them after I knew it was working so I didn't push them to GitHub)
+        file_holder = agent.get("http://feed.omgili.com/5Rh5AMTrc4Pv/mainstream/posts/#{link.href}")
+        file_holder.save("test/files/downloads/#{link.text}")
+        # if iterations <= 1
+        #   break
+        # end
+        # iterations -= 1
+      end
+    end
+  end
 end
 
 
-zipped_files = RubyInterviewStarter::GetThoseFiles.new
-zipped_files.unzip('test/files/news.zip', 'test/files/extract')
+# zipped_files = RubyInterviewStarter::GetThoseFiles.new
+# zipped_files.unzip('test/files/news.zip', 'test/files/extract')
+
+url_files = RubyInterviewStarter::DownloadFiles.new
+url_files.scrape
