00dbcc3ebd436412c0f0e17ee903ca73893d5d00
Joon Park <kudoku@Joons-MacBook-Pro.local>
2016-05-15 15:17:13 -0600

    working zip extract

--- lib/news_extractor_test.rb
+++ lib/news_extractor_test.rb
@@ -1,14 +1,39 @@
+require 'rubygems'
 require 'zip'
+# require 'pry' #uncomment to debug
 
 module NewsExtractorTest
 	class ExtractZip
+		class << self
+			attr_accessor :counter
+		end
+		self.counter = 0
 
-		def unzip(file_path)
-			Zip::ZipFile.open(file_path) { |zip_file|
-		    zip_file.extract(f, "/test/files/extract") unless File.exist?("/test/files/extract")
-		  }
+		def self.unzip(file_path, options: {})
+			extract_path = options.fetch( :extract_path, File.dirname(file_path) + "/extract/" ) 
+
+			Zip::File.open(file_path) do |zip_file|
+
+		    zip_file.each do |file|
+	        unless File.exist?(extract_path + file.name)
+			      puts "extracting #{file.name} to #{@extract_path}"
+			      self.counter += 1
+
+	        	f_path = File.join(extract_path, file.name)
+			      FileUtils::mkdir_p(File.dirname(f_path))
+			      zip_file.extract(file, f_path) 
+			    end
+			  end
+		    puts "extracted #{self.counter} files"
+
+		  end
+
 		end
 
 
 	end
 end
+
+file_path = File.expand_path('../../test/files/news.zip', __FILE__)
+
+NewsExtractorTest::ExtractZip.unzip( file_path ) #supply abs file path and an optional {extract_path:}. default xtract path is test/files/extract
