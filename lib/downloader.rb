b8e6cd5dd07e417c177671a49750028690df7f3f
Joon Park <kudoku@Joons-MacBook-Pro.local>
2016-05-15 21:11:38 -0600

    working file download

---
+++ lib/downloader.rb
@@ -0,0 +1,72 @@
+require 'fileutils'
+require 'net/http'
+require 'pry'
+
+module Downloader
+
+	class Connection
+
+		def initialize(endpoint)
+			@endpoint = endpoint
+			@uri = URI.parse(endpoint)
+	    @http = Net::HTTP.new(@uri.host, @uri.port)
+		end
+
+		def get_resources(type: type)
+			@http.start do |http|
+				request = Net::HTTP::Get.new @uri
+				response = http.request request
+				@resources = response.body.scan(/href="(.+?).zip"/).flatten
+			end
+
+			@resources.map! do |r|
+				@endpoint + r + "." + type.to_s
+			end
+		end
+
+		def get(dir, thread_count: 50)
+			Downloader::Connection.create_dir(dir)
+
+			@resources.each do |resource|
+				file_name = resource.split('/')[-1]
+				Downloader::Connection.download(file_name, @uri)
+			end
+
+		end
+
+		def self.download(filename, uri)
+			Net::HTTP.start(uri.host, uri.port) do |http|
+	      request = Net::HTTP::Get.new uri.request_uri
+	      http.read_timeout = 500
+	      http.request request do |response|
+	        open filename, 'w' do |io|
+	          response.read_body do |chunk|
+	            io.write chunk	
+	          end
+	        end
+	      end
+	    end
+		end
+
+		def self.create_dir(dirname)
+		  unless Dir.exists?(dirname)
+		    FileUtils::mkdir_p(dirname)
+		  end
+		  Dir.chdir(dirname)
+		end
+
+	end
+
+
+
+end
+
+url = "http://feed.omgili.com/5Rh5AMTrc4Pv/mainstream/posts/"
+
+connection = Downloader::Connection.new(url)
+
+connection.get_resources(type: :zip)
+
+destination_path = File.expand_path("../../test/files/downloads", __FILE__)
+
+connection.get( destination_path )
