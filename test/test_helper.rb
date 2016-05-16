a0a4811f01c2c884c59f76eab8436d3ec5ef8c61
Joon Park <kudoku@Joons-MacBook-Pro.local>
2016-05-16 04:09:34 -0600

    tests start

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,7 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'ruby_interview_starter'
+require 'extractor'
+require 'publish'
+require 'downloader'
 
 require 'minitest/autorun'
