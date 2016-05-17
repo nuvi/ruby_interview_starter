d18e6f2d1951b52284efb914a93da0815a627512
jrichocean <jrichocean@gmail.com>
2016-05-16 18:33:48 -0600

    UPDATES cli script to run the gem files

--- lib/ruby_interview_starter.rb
+++ lib/ruby_interview_starter.rb
@@ -1,6 +1,12 @@
-require "ruby_interview_starter/version"
-
+require_relative "ruby_interview_starter/version"
+require_relative "ruby_bitly_downloader"
+require_relative "ruby_bitly_unzipper"
 
 module RubyInterviewStarter
+
+  def self.execute
+    download_files = RubyBitlyDownloader.new.main
+    unzip_and_store = RubyBitlyUnzipper.new.main
+  end
 
 end
